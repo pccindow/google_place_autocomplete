d6fd98674aab2cbd60294c5216484c21e87bde65
callac <pccindo78@msn.com>
2018-03-21 22:17:42 +0100

    First commit

---
+++ test/system/autocompletes_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class AutocompletesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit autocompletes_url
+  #
+  #   assert_selector "h1", text: "Autocomplete"
+  # end
+end
