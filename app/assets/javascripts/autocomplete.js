592215b3c0292aed8340918f50a60f0e1b7ec8c6
callac <pccindo78@msn.com>
2018-03-22 00:15:22 +0100

    test2_if_google_place_autocoplete_works_in_production

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$( document ).on('turbolinks:load', function() {
 function initializeAutocomplete(id) {
     var element = document.getElementById(id);
     if (element) {
